6b74cacd8477a12f0ab6217f12662a539bf56a5b
tapuks <david_berdiell@hotmail.com>
2021-06-24 19:26:06 +0000

    finish

--- src/main.py
+++ src/main.py
@@ -8,7 +8,11 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Person
+from sqlalchemy import desc
+from operator import attrgetter
+
+
 #from models import Person
 
 app = Flask(__name__)
@@ -38,6 +42,46 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+# @app.route('/all', methods=['GET'])
+# def get_all():
+#     grandpas = Abuelo.query.order_by(desc(Abuelo.edad)).all()
+#     fathers = Padre.query.order_by(desc(Padre.edad)).all()
+#     currents_generations = Generacion_actual.query.order_by(desc(Generacion_actual.edad)).all()
+    
+#     newArray = []
+#     for father in fathers: 
+#         newArray.append(father.serialize())
+#     for grandpa in grandpas: 
+#         newArray.append(grandpa.serialize())
+#     for current in currents_generations: 
+#         newArray.append(current.serialize())
+        
+#     print("newarray:", newArray)
+#     newArray.sort(key=lambda person: person.get("edad"), reverse=True)
+   
+
+
+    return jsonify(newArray), 200
+
+@app.route('/all', methods=['GET'])
+def get_all():
+    people = Person.query.order_by(desc(Person.age)).all()
+    array = []
+    for person in people:
+        array.append(person.serialize()) 
+
+    return jsonify(array),200
+
+
+@app.route('/person/<int:id>', methods=['GET'])
+def show_person(id):
+    person = Person.query.filter_by(id = id).first()
+    father = Person.query.filter_by(id = person.parent_id).first()
+    children = Person.query.filter_by(parent_id = id).first()
+    family = {"persona": person.serialize(), "padre": father.serialize(), "hijo": children.serialize()}
+    return jsonify(family),200
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -17,3 +17,87 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Person(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120))
+    last_name = db.Column(db.String(80))
+    age = db.Column(db.Integer)
+    parent_id = db.Column(db.Integer, db.ForeignKey('person.id'))
+    parent = db.relationship('Person')
+
+
+    def __repr__(self):
+        return '<Person %s' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "last name": self.last_name,
+            "age": self.age,
+            "parent_id": self.parent_id,
+            # do not serialize the password, its a security breach
+        }
+
+# class Abuelo(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     nombre = db.Column(db.String(120))
+#     apellido = db.Column(db.String(80))
+#     edad = db.Column(db.Integer)
+
+#     def __repr__(self):
+#         return '<Abuelo %r>' % self.id
+        
+#     def serialize(self):
+#         return {
+#             "id": self.id,
+#             "nombre": self.nombre,
+#             "apellido": self.apellido,
+#             "edad": self.edad,
+            
+#             # do not serialize the password, its a security breach
+#         }
+
+    
+# class Padre(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     nombre = db.Column(db.String(120))
+#     apellido = db.Column(db.String(80))
+#     edad = db.Column(db.Integer)
+#     abuelo_id = db.Column(db.Integer, db.ForeignKey('abuelo.id'))
+#     abuelo = db.relationship('Abuelo')
+
+
+#     def __repr__(self):
+#         return '<Padre %r>' % self.id
+
+#     def serialize(self):
+#         return {
+#             "id": self.id,
+#             "nombre": self.nombre,
+#             "apellido": self.apellido,
+#             "edad": self.edad,
+#             "abuelo_id": self.abuelo_id,
+#             # do not serialize the password, its a security breach
+#         }
+
+# class Generacion_actual(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     nombre = db.Column(db.String(120), unique=False, nullable=False)
+#     apellido = db.Column(db.String(80), unique=False, nullable=False)
+#     edad = db.Column(db.Integer)
+#     padre_id = db.Column(db.Integer, db.ForeignKey('padre.id'))
+#     padre = db.relationship('Padre')
+#     def __repr__(self):
+#         return '<Generacion_actual %r>' % self.nombre
+
+#     def serialize(self):
+#         return {
+#             "id": self.id,
+#             "nombre": self.nombre,
+#             "apellido": self.apellido,
+#             "edad": self.edad,
+#             "padre_id": self.padre_id
+#             # do not serialize the password, its a security breach
+#         }
